ea9829ae451420113dfaf303060eb1bc9026b61f
Rafayel Khachatryan <rafar2005@yandex.ru>
2025-03-09 11:01:32 +0200

    Terminal delete issues

--- src/main/kotlin/com/rafengimprove/miniairport/controller/TerminalController.kt
+++ src/main/kotlin/com/rafengimprove/miniairport/controller/TerminalController.kt
@@ -3,11 +3,7 @@ package com.rafengimprove.miniairport.controller
 import com.rafengimprove.miniairport.model.dto.TerminalDto
 import com.rafengimprove.miniairport.service.TerminalService
 import org.slf4j.LoggerFactory
-import org.springframework.web.bind.annotation.DeleteMapping
-import org.springframework.web.bind.annotation.PathVariable
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/v1/terminal")
@@ -15,7 +11,7 @@ class TerminalController(val terminalService: TerminalService) {
     private val log = LoggerFactory.getLogger(TerminalController::class.java)
 
     @PostMapping
-    fun save(terminal: TerminalDto): TerminalDto {
+    fun save(@RequestBody terminal: TerminalDto): TerminalDto {
         log.debug("Save a new terminal")
         return terminalService.save(terminal)
     }
